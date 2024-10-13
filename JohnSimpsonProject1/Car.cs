9d7d6022ed9dcc9f739fe18025c6e8d989ba986c
John D Simpson <jsimpso9@my.westga.edu>
2024-10-13 11:23:16 -0400

    Created Cars class and implemented some basic details, car also has gained a constructor.

---
+++ JohnSimpsonProject1/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JohnSimpsonProject1
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double MPG { get; set; }
+        public decimal Price { get; set; }
+
+        public Car(string make, string model, double mpg, decimal price)
+        {
+            if (string.IsNullOrWhiteSpace(make))
+            {
+                throw new ArgumentNullException(nameof(make));
+            }
+
+            if (string.IsNullOrWhiteSpace(model))
+            {
+                throw new ArgumentException("Model cannot be null or just whitespace!");
+            }
+
+            if (mpg <= 0)
+            {
+                throw new ArgumentException("Miles Per Gallon cannot be less than zero!");
+            }
+
+            if (price <= 0)
+            {
+                throw new ArgumentException("Price must be greater than zero!");
+            }
+
+            Make = make;
+            Model = model;
+            MPG = mpg;
+            Price = price;
+        }
+    }
+}

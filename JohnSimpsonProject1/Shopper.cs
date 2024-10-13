c2f06a5e0bce8f91cfe51fdc513d49d5a4a946e3
John D Simpson <jsimpso9@my.westga.edu>
2024-10-13 17:17:25 -0400

    Added shopper class. Added basic details about shopper, can update via constructor. can also determine if car is within budget.

---
+++ JohnSimpsonProject1/Shopper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JohnSimpsonProject1
+{
+    public class Shopper
+    {
+        private string Name { get; set; }
+        private decimal MoneyAvailable { get; set; }
+
+        private List<Car> Cars;
+
+        public Shopper(string name, decimal moneyAvailable)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name cannot be null or white space");
+            }
+
+            if (moneyAvailable < 0)
+            {
+                throw new ArgumentException("Money cannot be less than zero");
+            }
+            Name = name;
+            MoneyAvailable = moneyAvailable;
+            Cars = new List<Car>();
+        }
+
+        public Boolean CanPurchase(Car car)
+        {
+            if (car == null)
+            {
+                throw new ArgumentException("Car cannot be null");
+            }
+
+            var vehicleCost = CarLot.GetTotalCostsOfPurchase(car);
+            return vehicleCost <= MoneyAvailable;
+        }
+    }
+}
